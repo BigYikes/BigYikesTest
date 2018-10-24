246c781ab958e0432ed019ac4aa868ee08ddf31a
Andy van Sundert <andy482382@gmail.com>
2018-10-24 09:26:48 +0200

    addes images

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 let button = document.getElementById("rotateBtn");
 let body = document.querySelector('body');
+let CatButton = document.getElementById("rotateCat");
+let cat = document.querySelector('main');
 
 button.addEventListener('dblclick', () => {
 	body.classList.add('rotate')
