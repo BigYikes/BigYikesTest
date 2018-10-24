364768aa8273b75cc5e6125959ded0bbdfcb6b78
Andy van Sundert <andy482382@gmail.com>
2018-10-24 09:06:45 +0200

    added extra's

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 let button = document.getElementById("rotateBtn");
 let body = document.querySelector('body');
 
-/*button.addEventListener('dblclick', () => {
-	document.body.setAttribute( "style", "-webkit-transform: rotate(-180deg);")
-}); */
-
 button.addEventListener('dblclick', () => {
 	body.classList.add('rotate')
 });
