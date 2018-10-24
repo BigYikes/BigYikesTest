d710c8c09e57801c0380907dda126aa3905b91e9
Andy van Sundert <andy482382@gmail.com>
2018-10-24 08:55:51 +0200

    First commit
    added index.html, style.css and script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let button = document.getElementById("rotateBtn");
+let body = document.querySelector('body');
+
+/*button.addEventListener('dblclick', () => {
+	document.body.setAttribute( "style", "-webkit-transform: rotate(-180deg);")
+}); */
+
+button.addEventListener('dblclick', () => {
+	body.classList.add('rotate')
+});
